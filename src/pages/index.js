99ffc03ce291c6d5edb64fa8b9c64c08d319e3ba
Daniel_hi <daniel@Daniel-hideMacBook-Pro.local>
2023-10-11 10:49:13 +0800

    new code

---
+++ src/pages/index.js
@@ -0,0 +1,26 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import Link from 'next/link'
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home({ formattedDate }) {
+  return (
+    <>
+      <h1>Static page</h1>
+      <p>This page is static. It was built on {formattedDate}.</p>
+      <p>
+        <Link href="/ssr">View a server-side rendered page.</Link>
+      </p>
+    </>
+  );
+}
+
+export async function getStaticProps() {
+  const buildDate = Date.now();
+  const formattedDate = new Intl.DateTimeFormat("en-US", {
+    dateStyle: "long",
+    timeStyle: "long",
+  }).format(buildDate);
+
+  return { props: { formattedDate } };
+}
